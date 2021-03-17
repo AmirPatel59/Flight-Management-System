430956856f8dc7ed7e09baf7f1a6f43c05e1667f
shubhampurkar <shubham-lahanu.purkar@capgemini.com>
2021-03-17 22:13:02 +0530

    User

--- src/main/java/com/cg/flightmgmt/exception/UserNotFoundException.java
+++ src/main/java/com/cg/flightmgmt/exception/UserNotFoundException.java
@@ -1,5 +1,10 @@
 package com.cg.flightmgmt.exception;
 
-public class UserNotFoundException extends Exception {
 
+
+public class UserNotFoundException extends Exception {
+	
+	public UserNotFoundException(String m) {
+		super (m);
+	}
 }
